2c84291bbceaf1954aca37be11f601d19abda247
saltire sable <saltiresable@gmail.com>
2013-05-07 15:57:39 -0400

    Initial commit with skeleton test framework

---
+++ test/test_datafile.py
@@ -0,0 +1,14 @@
+import unittest
+
+from advengine.datafile import DataFile
+
+
+class Test(unittest.TestCase):
+
+
+    def test_initialize_datafile(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
