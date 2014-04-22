ad959dc766a68fc3561835d9ba6d6c9507a65d2f
saltire sable <saltiresable@gmail.com>
2014-04-22 00:16:46 -0400

    Convert controls to dict

--- advengine/adventure.py
+++ advengine/adventure.py
@@ -40,6 +40,34 @@ class Adventure:
         return messages
 
 
+    def get_control_actions(self, control):
+        """Run each set of tests for this control, and if all tests are true
+        for any set of tests, return the 'then' actions, otherwise 'else'."""
+        def test_is_true(test):
+            # return opposite if test is preceded by a bang
+            neg, test = (True, test[1:]) if test[0] == '!' else (False, test)
+
+            method, args = test.split()[0], test.split()[1:]
+            return getattr(self.tests, method)(*args) ^ neg
+
+        results = (control['true_results'] if not control['conds']
+                   or any(all(test_is_true(test) for test in cond) for cond in control['conds'])
+                   else control['false_results'])
+
+        actions = []
+
+        for result in results:
+            if isinstance(result, dict):
+                # run tests for this control and return its actions
+                actions.extend(self.get_control_actions(control))
+            else:
+                # return the actions
+                action, args = result.split()[0], result.split()[1:]
+                actions.append((action, args))
+
+        return actions
+
+
     def do_actions(self, controls):
         """Evaluate each of the controls, run any actions and return
         any messages. End execution if 'done', end game if 'gameover'."""
