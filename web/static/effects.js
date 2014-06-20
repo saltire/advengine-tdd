f366b4cf2ed80eae07c89aa7deab435a550317f1
saltire sable <x@saltiresable.com>
2014-06-20 12:37:06 -0400

    More restyling in progress

--- web/static/effects.js
+++ web/static/effects.js
@@ -1,11 +1,5 @@
 $(function() {
 	$('body')
-	.on('click', '.objects li', function() {
-		$('.details', this).slideToggle();
-	})
-	.on('click', '.details', function(e) {
-		e.stopPropagation();
-	})
 	
 	// expand/collapse controls
 	.on('click', '.control-header', function() {
